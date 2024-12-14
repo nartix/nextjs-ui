0a0827726304cbc7c93951ccf1193eba2ff6ab04
feroz <nartix@gmail.com>
2024-12-14 18:12:54 +0000

    improvements locale middleware

--- apps/appone/middlewares/nextintl-middleware.ts
+++ apps/appone/middlewares/nextintl-middleware.ts
@@ -3,13 +3,12 @@
 import createMiddleware from 'next-intl/middleware';
 import { MiddlewareHandler } from '@/types/middleware-handler';
 import { routing } from '@/i18n/routing';
-import { isLocaleSupported } from './locale-logger-middleware';
+import { isLocaleSupported, isPublicPath } from './locale-logger-middleware';
 
 export const nextIntlMiddleware: MiddlewareHandler = async (req, res) => {
-  if (isLocaleSupported(req.nextUrl.pathname)) {
-    console.log(`Locale is supported: ${req.nextUrl.pathname}`);
+  // if locale is supported or public page, then no need to call next-intl middleware
+  if (isLocaleSupported(req.nextUrl.pathname) || isPublicPath(req.nextUrl.pathname)) {
     return { response: res, next: true };
   }
-  console.log('next intl middleware');
   return { response: createMiddleware(routing)(req), next: true };
 };
--- apps/appone/middlewares/test-middleware.ts
+++ apps/appone/middlewares/test-middleware.ts
@@ -2,6 +2,7 @@
 
 import { NextResponse } from 'next/server';
 import { MiddlewareHandler } from '@/types/middleware-handler';
+import { isPublicPath, isLocaleSupported } from '@/middlewares/locale-logger-middleware';
 
 export const testMiddleware: MiddlewareHandler = async (req, res) => {
   //set cookie using res
@@ -15,6 +16,15 @@ export const testMiddleware: MiddlewareHandler = async (req, res) => {
   //   const redirectResponse = NextResponse.redirect(new URL('/login', req.url));
   //   redirectResponse.cookies.set('redirected', 'true'); // Example of setting cookies on redirect
   //   return { response: redirectResponse, next: false };
+  // }
+
+  //
+  // if (isPublicPath(req.nextUrl.pathname)) {
+  //   console.log('public and locale path===================');
+  // }
+
+  // if (isLocaleSupported(req.nextUrl.pathname)) {
+  //   console.log('supported locale===================');
   // }
 
   return { response: res!, next: true };
