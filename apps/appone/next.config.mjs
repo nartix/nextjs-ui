084b1cc0dfab2823e4371d29c28c2b1b3f16dfd6
Feroz <feroz@hotmail.ca>
2025-06-05 09:25:49 +0000

    added standalone output to nextjs config

--- apps/appone/next.config.mjs
+++ apps/appone/next.config.mjs
@@ -12,6 +12,7 @@ const nextConfig = {
   experimental: {
     optimizePackageImports: ['@mantine/core', '@mantine/hooks'],
   },
+  output: 'standalone',
 };
 
 export default withNextIntl(nextConfig);
