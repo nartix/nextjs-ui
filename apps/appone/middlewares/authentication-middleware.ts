3ec825b29ceae37da590ed2bde8e00c65d9e4297
Feroz <feroz@hotmail.ca>
2025-06-06 09:53:38 +0000

    cleanup

--- apps/appone/middlewares/authentication-middleware.ts
+++ apps/appone/middlewares/authentication-middleware.ts
@@ -6,17 +6,7 @@ import { getClientIp } from '@/lib/get-client-ip';
 import { MiddlewareFactory } from '@nartix/next-middleware-chain/src';
 import { NextRequest, NextFetchEvent, NextResponse } from 'next/server';
 
-// // way to overload sessionObj with custom properties
-// export const authenticationMiddleware: MiddlewareHandler = async (req, res) => {
-//   console.log('authentication middleware url path', req.nextUrl.pathname);
-//   const sessionObj = {
-//     ipAddress: getClientIp(req.headers),
-//     urlPath: req.nextUrl.pathname,
-//     userAgent: req.headers.get('user-agent') || '',
-//   };
-//   return await nextSecurityMiddleware(req, res, authConfig, sessionObj);
-// };
-
+// Overloading sessionObj with custom properties
 export const authenticationMiddlewareFactory: MiddlewareFactory = (next) => {
   return async (req: NextRequest, event: NextFetchEvent, response?: NextResponse) => {
     console.log('authentication middleware url path', req.nextUrl.pathname);
