29e0873acae52f27375ffe74e587ec36e9234392
Feroz <feroz@hotmail.ca>
2025-06-20 10:14:00 +0000

    removed csrf-context2.tsx added about page

---
+++ apps/appone/app/[locale]/(common)/about/page.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { getTranslations } from 'next-intl/server';
+import { getServerSession } from '@/app/[locale]/(auth)/get-server-session';
+import { SessionContainer } from '@/components/common/SessionContainer/SessionContainer';
+import { Text, Title } from '@mantine/core';
+
+export default async function About() {
+  const session = await getServerSession();
+  const user = session?.user;
+
+  const t = await getTranslations('HomePage');
+
+  const lorem =
+    'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos ullam, ex cum repellat alias ea nemo. Ducimus ex nesciunt hic ad saepe molestiae nobis necessitatibus laboriosam officia, reprehenderit, earum fugiat?';
+
+  return (
+    <SessionContainer justify='flex-start' align='center' pt='lg' direction="column" >
+      <Title order={4} className='max-w-2xl'>
+        About this NextJS project
+      </Title>
+
+      <Text size='md' className='max-w-2xl'>
+        {lorem}
+      </Text>
+    </SessionContainer>
+  );
+}
