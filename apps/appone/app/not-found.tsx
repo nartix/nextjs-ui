48e772ae0e7c09dd9768004577821ddf918e9750
Feroz <feroz@hotmail.ca>
2025-06-21 08:42:34 +0000

    getting the right error for /unknown.txt

--- apps/appone/app/not-found.tsx
+++ apps/appone/app/not-found.tsx
@@ -1,20 +1,21 @@
 // import { Link } from '@/i18n/routing';
+import { Link } from "@/i18n/routing";
 
-// export default function NotFound() {
-//   return (
-//     <html lang='en'>
-//       <head>
-//         <title>Page Not Found</title>
-//         <meta name='viewport' content='width=device-width, initial-scale=1' />
-//         <meta charSet='UTF-8' />
-//       </head>
-//       <body>
-//         <div style={{ textAlign: 'center', marginTop: '20vh' }}>
-//           <h1>404 - Page Not Found</h1>
-//           <p>The page you are looking for does not exist.</p>
-//           <Link href='/'>Go back to home</Link>
-//         </div>
-//       </body>
-//     </html>
-//   );
-// }
+export default function NotFound() {
+  return (
+    <html lang='en'>
+      <head>
+        <title>Page Not Found</title>
+        <meta name='viewport' content='width=device-width, initial-scale=1' />
+        <meta charSet='UTF-8' />
+      </head>
+      <body>
+        <div style={{ textAlign: 'center', marginTop: '20vh' }}>
+          <h1>404 - Page Not Found</h1>
+          <p>The page you are looking for does not exist.</p>
+
+        </div>
+      </body>
+    </html>
+  );
+}
